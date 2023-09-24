32e3549021df014aaa6bc2553bf1e3cbf1828a82
JRBros2346 <jrbros2346@gmail.com>
2023-09-24 20:04:11 +0530

    changes

--- main.cpp
+++ main.cpp
@@ -2,73 +2,18 @@
 
 #include"vector.hpp"
 
-int main()
-{
-    std::cout << "For Integer data_type" << std::endl;
- 
-    Lib::Vector<int> v(6);
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
-    v.push(9);
-    v.push(10);
-    v.push(11);
-    v.push(12);
- 
-    // Function called to display element in
-    // vector in v
-    std::cout << "Element in vector v : < ";
-    for (auto e = v.begin(); e < v.end(); e++) std::cout << *e << ' ';
-    std::cout << '>' << std::endl;
- 
-    // Print the size of vector v
-    std::cout << "size: " << v.size() << std::endl;
- 
-    // Print Element at index 2
-    std::cout << "v[2]: " << v[2] << '\n';
-    std::cout << "v.at(2): " << v.at(2) << '\n';
- 
-    // Pop an element and print it
-    std::cout << "Popped Element: " << v.pop() << '\n';
-    std::cout << "< ";
-    for (auto i : v) std::cout << i << ' ';
-    std::cout << '>' << std::endl;
- 
-    std::cout << std::endl;
- 
-    std::cout << "For Char data_type" << std::endl;
- 
-    Lib::Vector<char> c;
-    c.push('a');
-    c.push('b');
-    c.push('c');
-    c.push('d');
-    c.push('e');
-    c.push('f');
-    c.push('g');
-    c.push('h');
- 
-    // Function called to display element in
-    // vector in c
-    std::cout << "Element in vector c : < ";
-    for (auto e = c.begin(); e < c.end(); e++) std::cout << *e << ' ';
-    std::cout << '>' << std::endl;
- 
-    // Print the size of vector c
-    std::cout << "size: " << c.size() << '\n';
- 
-    // Print Element at index 2
-    std::cout << "c[2]: " << c[2] << '\n';
-    std::cout << "c.at(2): " << c.at(2) << '\n';
- 
-    // Pop an element and print it
-    std::cout << "pop: " << c.pop(2) << '\n';
-    std::cout << "push: z" << c.push('z', 3) << '\n';
-    // Function called to display element in
-    // vector in c
-    std::cout << "Element in vector c : < ";
-    for (auto e = c.begin(); e < c.end(); e++) std::cout << *e << ' ';
-    std::cout << '>' << std::endl;
-    return 0;
+int main() {
+	Lib::Vector<int> v;
+	v.push(1);
+	v.push(100);
+	v.emplace(5);
+	v.emplace(6);
+	v.pop();
+	v.emplace(10);
+	v.display();
+
+	int sum = 0;
+	for (const auto& e : v)
+		sum += e;
+	std::cout << "Sum: " << sum;
 }
--- vector.hpp
+++ vector.hpp
@@ -1,120 +1,306 @@
 #pragma once
 
-#include"iterator.hpp"
-// #include<cstdexc>
+#include<iostream>
 
 namespace Lib {
 	template<typename T>
 	class Vector {
-		const static auto DEFAULT_CAPACITY = 1ULL;
-		const static auto RESIZE_FACTOR = 2ULL;
-		
-		unsigned long long SIZE;
-		unsigned long long CAPACITY;
-		T* DATA;
+		const static size_t DEFAULT_CAPACITY = 1;
+		const static size_t RESIZE_FACTOR = 2;
+
+		size_t SIZE = 0;
+		size_t CAPACITY = 0;
+		T* DATA = nullptr;
 		public:
 			explicit Vector();
-			explicit Vector(Vector<T>&);
-			explicit Vector(unsigned long long);
+			explicit Vector(size_t);
+			explicit Vector(Vector<T>&&);
+			explicit Vector(const Vector<T>&);
 			~Vector();
-			
-			T& operator[](unsigned long long);
-			T& at(unsigned long long);
-			
-			void resize();
-			
-			unsigned long long push(T, unsigned long long);
-			unsigned long long push(T);
-			T pop(unsigned long long);
+
+			void push(T&&);
+			void push(const T&);
+			template<typename... Args>
+			T& emplace(Args&&... args);
 			T pop();
 			
+			void resize();
 			void clear();
-			unsigned long long size() const;
-			unsigned long long capacity() const;
+			
+			size_t size() const;
+			size_t capacity() const;
 			T* data() const;
 			
-			Iterator<T> begin() const;
-			Iterator<T> end() const;
+			class Iter {
+				T* ptr;
+				public:
+					Iter();
+					Iter(T*);
+					
+					T& operator*();
+					T* operator->();
+					
+					Iter& operator++();
+					Iter operator++(int);
+					Iter& operator--();
+					Iter operator--(int);
+					
+					bool operator==(Iter&);
+					bool operator!=(Iter&);
+					bool operator<=(Iter&);
+					bool operator>=(Iter&);
+					bool operator<(Iter&);
+					bool operator>(Iter&);
+			};
+			
+			Iter begin();
+			Iter end();
+			Iter rbegin();
+			Iter rend();
+			
+			class ConstIter {
+				T* ptr;
+				public:
+					ConstIter();
+					ConstIter(T*);
+					
+					const T& operator*() const;
+					const T* operator->() const;
+					
+					ConstIter& operator++();
+					ConstIter operator++(int);
+					ConstIter& operator--();
+					ConstIter operator--(int);
+					
+					bool operator==(ConstIter&);
+					bool operator!=(ConstIter&);
+					bool operator<=(ConstIter&);
+					bool operator>=(ConstIter&);
+					bool operator<(ConstIter&);
+					bool operator>(ConstIter&);
+			};
+			
+			const ConstIter cbegin() const;
+			const ConstIter cend() const;
+			const ConstIter crbegin() const;
+			const ConstIter crend() const;
+			
+			void display();
 	};
-	
+
 	template<typename T>
 	Vector<T>::Vector()
-			: CAPACITY(DEFAULT_CAPACITY),
-			  DATA(new T[DEFAULT_CAPACITY]),
-			  SIZE(0) {
+			: SIZE(0),
+			  CAPACITY(DEFAULT_CAPACITY) {
+		DATA = (T*)::operator new(DEFAULT_CAPACITY * sizeof(T));
 	}
 	template<typename T>
-	Vector<T>::Vector(Vector<T>& other)
-			: DATA(other.data()),
-			  SIZE(other.size()),
-			  CAPACITY(other.capacity()) {
-	}
-	template<typename T>
-	Vector<T>::Vector(unsigned long long capacity)
-			: DATA(new T[capacity]),
-			  SIZE(0),
+	Vector<T>::Vector(size_t capacity)
+			: SIZE(0),
 			  CAPACITY(capacity) {
+		DATA = (T*)::operator new(capacity * sizeof(T));
 	}
 	template<typename T>
-	Vector<T>::~Vector() { delete[] DATA; }
-	
-	template<typename T>
-	T& Vector<T>::operator[](unsigned long long pos) {
-		return DATA[pos];
+	Vector<T>::Vector(Vector<T>&& other)
+			: SIZE(other.SIZE),
+			  CAPACITY(other.CAPACITY) {
+		DATA = other.DATA;
+		
+		other.SIZE = 0;
+		other.CAPACITY = 0;
+		other.DATA = nullptr;
 	}
 	template<typename T>
-	T& Vector<T>::at(unsigned long long pos) {
-		if (pos < SIZE) 
-			return DATA[pos];
-		else
-			std::invalid_argument("Out of bounds!");
+	Vector<T>::Vector(const Vector<T>& other)
+			: SIZE(other.SIZE),
+			  CAPACITY(other.CAPACITY) {
+		DATA = ::operator new(other.CAPACITY * sizeof(T));
+		for (size_t i = 0; i < other.SIZE; i++)
+			DATA[i] = other.DATA[i];
+	}
+	template<typename T>
+	Vector<T>::~Vector() {
+		clear();
+		::operator delete(DATA, CAPACITY * sizeof(T));
+	}
+
+	template<typename T>
+	void Vector<T>::push(const T& val) {
+		if (SIZE == CAPACITY)
+			resize();
+		DATA[SIZE++] = val;
+	}
+	template<typename T>
+	void Vector<T>::push(T&& val) {
+		if (SIZE == CAPACITY)
+			resize();
+		DATA[SIZE++] = std::move(val);
+	}
+	template<typename T>
+	template<typename... Args>
+	T& Vector<T>::emplace(Args&&... args) {
+		if (SIZE == CAPACITY)
+			resize();
+		new(&DATA[SIZE]) T(std::move(args)...);
+		return DATA[SIZE++];
+	}
+	template<typename T>
+	T Vector<T>::pop() {
+		T val = std::move(DATA[--SIZE]);
+		DATA[SIZE].~T();
+		return val;
 	}
 	
 	template<typename T>
 	void Vector<T>::resize() {
 		CAPACITY *= RESIZE_FACTOR;
-		T* temp = DATA;
-		DATA = new T[CAPACITY];
-		for (unsigned long long i = 0; i < SIZE; i++) DATA[i] = temp[i];
-	}
-
-	template<typename T>
-	unsigned long long Vector<T>::push(T val, unsigned long long pos) {
-		if(SIZE == CAPACITY) this->resize();
-		for (auto i = SIZE; i > pos; i--) 
-			DATA[i+1] = DATA[i];
-		DATA[pos] = val;
-		return SIZE++;
+		T* block = (T*)::operator new(CAPACITY * sizeof(T));
+		for (size_t i = 0; i < SIZE; i++)
+			block[i] = std::move(DATA[i]);
+		::operator delete(DATA, CAPACITY * sizeof(T));
+		DATA = block;
+		block = nullptr;
 	}
 	template<typename T>
-	unsigned long long Vector<T>::push(T val) {
-		if(SIZE == CAPACITY) this->resize();
-		DATA[SIZE] = val;
-		return SIZE++;
+	void Vector<T>::clear() {
+		for (size_t i = 0; i < SIZE; i++)
+			DATA[i].~T();
+		SIZE = 0;
 	}
-
-	template<typename T>
-	T Vector<T>::pop(unsigned long long pos) {
-		T val = DATA[pos];
-		for (auto i = pos; i < SIZE-1; i++)
-			DATA[i] = DATA[i+1];
-		SIZE--;
-		return val;
-	}
-	template<typename T>
-	T Vector<T>::pop() { return DATA[--SIZE]; }
 	
 	template<typename T>
-	void Vector<T>::clear() { SIZE = 0; }
+	size_t Vector<T>::size() const { return SIZE; }
 	template<typename T>
-	unsigned long long Vector<T>::size() const { return SIZE; }
-	template<typename T>
-	unsigned long long Vector<T>::capacity() const { return CAPACITY; }
+	size_t Vector<T>::capacity() const { return CAPACITY; }
 	template<typename T>
 	T* Vector<T>::data() const { return DATA; }
 	
 	template<typename T>
-	Iterator<T> Vector<T>::begin() const { return Iterator<T>(DATA); }
+	Vector<T>::Iter::Iter() : ptr(nullptr) {
+	}
 	template<typename T>
-	Iterator<T> Vector<T>::end() const { return Iterator<T>(DATA + SIZE); }
+	Vector<T>::Iter::Iter(T* p) : ptr(p) {
+	}
+	
+	template<typename T>
+	T& Vector<T>::Iter::operator*() {
+		return *ptr;
+	}
+	template<typename T>
+	T* Vector<T>::Iter::operator->() {
+		return ptr;
+	}
+	
+	template<typename T>
+	typename Vector<T>::Iter& Vector<T>::Iter::operator++() {
+		++ptr;
+		return *this;
+	}
+	template<typename T>
+	typename Vector<T>::Iter Vector<T>::Iter::operator++(int) {
+		Iter tmp = *this;
+		++ptr;
+		return tmp;
+	}
+	template<typename T>
+	typename Vector<T>::Iter& Vector<T>::Iter::operator--() {
+		--ptr;
+		return *this;
+	}
+	template<typename T>
+	typename Vector<T>::Iter Vector<T>::Iter::operator--(int) {
+		Iter tmp = *this;
+		--ptr;
+		return tmp;
+	}
+	
+	template<typename T>
+	bool Vector<T>::Iter::operator==(Vector<T>::Iter& other) { return ptr == other.ptr; }
+	template<typename T>
+	bool Vector<T>::Iter::operator!=(Vector<T>::Iter& other) { return ptr != other.ptr; }
+	template<typename T>
+	bool Vector<T>::Iter::operator<=(Vector<T>::Iter& other) { return ptr <= other.ptr; }
+	template<typename T>
+	bool Vector<T>::Iter::operator>=(Vector<T>::Iter& other) { return ptr >= other.ptr; }
+	template<typename T>
+	bool Vector<T>::Iter::operator<(Vector<T>::Iter& other) { return ptr < other.ptr; }
+	template<typename T>
+	bool Vector<T>::Iter::operator>(Vector<T>::Iter& other) { return ptr > other.ptr; }
+	
+	template<typename T>
+	typename Vector<T>::Iter Vector<T>::begin() { return DATA; }
+	template<typename T>
+	typename Vector<T>::Iter Vector<T>::end() { return DATA + SIZE; }
+	template<typename T>
+	typename Vector<T>::Iter Vector<T>::rbegin() { return DATA + SIZE - 1; }
+	template<typename T>
+	typename Vector<T>::Iter Vector<T>::rend() { return DATA - 1; }
+	
+	template<typename T>
+	Vector<T>::ConstIter::ConstIter() : ptr(nullptr) {}
+	template<typename T>
+	Vector<T>::ConstIter::ConstIter(T* p) : ptr(p) {}
+	
+	template<typename T>
+	const T& Vector<T>::ConstIter::operator*() const {
+		return *ptr;
+	}
+	template<typename T>
+	const T* Vector<T>::ConstIter::operator->() const {
+		return ptr;
+	}
+	
+	template<typename T>
+	typename Vector<T>::ConstIter& Vector<T>::ConstIter::operator++(){
+		++ptr;
+		return *this;
+	}
+	template<typename T>
+	typename Vector<T>::ConstIter Vector<T>::ConstIter::operator++(int){
+		Iter tmp = *this;
+		++ptr;
+		return tmp;
+	}
+	template<typename T>
+	typename Vector<T>::ConstIter& Vector<T>::ConstIter::operator--(){
+		--ptr;
+		return *this;
+	}
+	template<typename T>
+	typename Vector<T>::ConstIter Vector<T>::ConstIter::operator--(int){
+		Iter tmp = *this;
+		--ptr;
+		return tmp;
+	}
+	
+	template<typename T>
+	bool Vector<T>::ConstIter::operator==(Vector<T>::ConstIter& other) { return ptr == other.ptr; }
+	template<typename T>
+	bool Vector<T>::ConstIter::operator!=(Vector<T>::ConstIter& other) { return ptr != other.ptr; }
+	template<typename T>
+	bool Vector<T>::ConstIter::operator<=(Vector<T>::ConstIter& other) { return ptr <= other.ptr; }
+	template<typename T>
+	bool Vector<T>::ConstIter::operator>=(Vector<T>::ConstIter& other) { return ptr >= other.ptr; }
+	template<typename T>
+	bool Vector<T>::ConstIter::operator<(Vector<T>::ConstIter& other) { return ptr < other.ptr; }
+	template<typename T>
+	bool Vector<T>::ConstIter::operator>(Vector<T>::ConstIter& other) { return ptr > other.ptr; }
+	
+	template<typename T>
+	const typename Vector<T>::ConstIter Vector<T>::cbegin() const { return DATA; }
+	template<typename T>
+	const typename Vector<T>::ConstIter Vector<T>::cend() const { return DATA + SIZE; }
+	template<typename T>
+	const typename Vector<T>::ConstIter Vector<T>::crbegin() const { return DATA + SIZE - 1; }
+	template<typename T>
+	const typename Vector<T>::ConstIter Vector<T>::crend() const { return DATA - 1; }
+	
+	template<typename T>
+	void Vector<T>::display() {
+		std::cout << "< ";
+		for (const auto& v : *this)
+			std::cout << v << ' ';
+		std::cout << '>' << std::endl;
+	}
+
 } // namespace Lib
